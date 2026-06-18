2fe3b59629d60aa8132f58270a01a043e8a9f919
tay-souf <83426032+tay-souf@users.noreply.github.com>
2026-06-18 05:26:45 +0100

    fix: resolve X509Certificate type collision in badCertificateCallback

--- lib/main.dart
+++ lib/main.dart
@@ -178,6 +178,6 @@ class _MyAppState extends State<MyApp> {
 class MyHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+    return super.createHttpClient(context)..badCertificateCallback = (cert, host, port) => true;
   }
 }
